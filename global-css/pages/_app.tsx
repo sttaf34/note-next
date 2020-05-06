1966443564803847a30de44de81a34ddc7f64bcd
Shinichi Kawamura <sttaf34@gmail.com>
2020-05-07 00:51:18 +0900

    コメント変更

--- global-css/pages/_app.tsx
+++ global-css/pages/_app.tsx
@@ -1,15 +1,15 @@
 /* eslint-disable react/jsx-props-no-spreading */
 
-import "../styles/global.css"
-
 import React from "react"
 import { AppProps } from "next/app"
+
+import "../styles/global.css"
 
 // _app.tsx でエクスポートしたコンポーネントは
 // すべてのページに共通するトップレベルのコンポーネントになるとのこと
 // グローバルな CSS のファイルは _app.tsx からのみインポートできる
 
-// TODO: ここの仕組みがよくわからない、返り値の型もあってないような
+// TODO: ここの仕組みがよくわかってない
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return <Component {...pageProps} />
 }
